da24c02cb5d946750fb09e2ae1ce11c69f393028
Daniel Bogusz <respown@gmail.com>
2025-03-26 16:06:24 +0100

    add movement lib

---
+++ apps/movement-library/src/app/page.tsx
@@ -0,0 +1,49 @@
+// import { Header } from "@t3-test/components/ui/Header";
+import { Panel } from "@t3-test/components/ui/Panel/Panel";
+import { time } from "console";
+import { ReactNode } from "react";
+// import Link from "next/link";
+// import { type ReactNode } from "react";
+
+const Panels = ({ children }: { children: ReactNode }) => {
+  return <div className="panels">{children}</div>;
+};
+
+export default function HomePage() {
+  return (
+    <Panels>
+      <Panel
+        title="1. Reduce your back pain with this exercise"
+        tags={["back pain", "shoulders"]}
+        duration={{time: 12, unit: "seconds", note: "you can repeat 3 times"}}
+        wtfFactor={{level: 0, note: "you are going to look fine doing this"}} 
+        summary="A small description about what is a butchers block and how it affects your life."
+        steps={[
+          "Make sure you seat in front of stable desk",
+          "Put your hands on the desk",
+          "Push your body below the desk",
+          "Hold for 10 seconds",
+          "Push your body up",
+          "You can repeat it 3 times from step 3"
+        ]}
+        presenter={{
+          name: "Kacper Dziuba - Trener Personalny (kalistenika)",
+          avatar: "",
+          link: "https://imperatordrazka.pl"
+        }}
+      />
+      {/* <Panel
+        title="Butchers block"
+        summary="A small description about what is a butchers block and how it affects your life."
+      />
+      <Panel
+        title="Butchers block"
+        summary="A small description about what is a butchers block and how it affects your life."
+      />
+      <Panel
+        title="Butchers block"
+        summary="A small description about what is a butchers block and how it affects your life."
+      /> */}
+    </Panels>
+  );
+}
