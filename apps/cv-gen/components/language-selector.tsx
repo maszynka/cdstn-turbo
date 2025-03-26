2b3c52ee4da4650954e224002a09017ede2d8a67
Daniel Bogusz <respown@gmail.com>
2025-03-26 18:10:43 +0100

    wip

--- apps/cv-gen/components/language-selector.tsx
+++ apps/cv-gen/components/language-selector.tsx
@@ -12,6 +12,7 @@ interface LanguageSelectorProps {
 
 const LANGUAGES = [
   { code: "en", name: "English" },
+  { code: "pl", name: "Polski" },
   { code: "es", name: "Español" },
   { code: "fr", name: "Français" },
   { code: "de", name: "Deutsch" },
